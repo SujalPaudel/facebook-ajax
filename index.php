f426d7c3679a495d23515e3486f8f7163eae26c3
Velar <Sujal Paudel>
2019-01-15 12:14:09 +0545

    basic styling

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+  <head>
+    <title>Comment Section</title>
+     <link href = "layout.css" rel = 'stylesheet'/>
+     <script type = "text/javascript" src = "/js/jquery.js"></script>
+     <script type = "text/javascript" src = "/js/script.js"></script>
+  </head>
+
+  <body>
+    <div class = "wrapper">
+      <div class = "page-data">
+        The page content lies over here
+      </div>
+
+      <div class = "comment-wrapper">
+        <h3 class = 'comment-title'>User Feedback...</h3>
+        <div class="comments-list">
+          <ul class = "comments-holder-ul">
+            <li class = "comment-holder" id = "_1">
+              <div class = "user-img">
+                <img src = "#" class = "user-img-pic" />
+              </div>
+
+              <h3 class="username-field">
+                Sujal Paudel
+              </h3>
+
+              <div class="comment-text">
+                The comment of person will go here
+              </div>
+              </li></ul>
+            <li class = "comment-holder" id = "_1"></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
